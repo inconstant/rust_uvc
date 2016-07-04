496673f996b47071aa1a03be63c9089a0e4d5a20
k.masatani <sonntag902@gmail.com>
2016-07-04 21:53:28 +0900

    use rscam and success capture video picture.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+extern crate rust_uvc;
+extern crate libc;
+extern crate rscam;
+
+use std::fs::File;
+use std::io::Write;
+use std::thread;
+use std::time::Duration;
+
+use rscam::{Camera, Config, ResolutionInfo};
+
 fn main() {
-    println!("Hello, world!");
+    let mut camera = Camera::new("/dev/video0").unwrap();
+
+    camera.start(&Config {
+        interval: (1, 30),      // 30 fps.
+        resolution: (1280, 720),
+        format: b"MJPG",
+        ..Default::default()
+    }).unwrap();
+
+    for i in 0..10 {
+        let frame = camera.capture().unwrap();
+        let mut file = File::create(&format!("frame-{}.jpg", i)).unwrap();
+        file.write_all(&frame[..]).unwrap();
+        thread::sleep(Duration::new(1, 0));
+    }
 }
+/*
+fn main() {
+    let camera = Camera::new("/dev/video0").unwrap();
+
+    for wformat in camera.formats() {
+        let format = wformat.unwrap();
+        println!("{:?}", format);
+
+        let resolutions = camera.resolutions(&format.format).unwrap();
+
+        if let ResolutionInfo::Discretes(d) = resolutions {
+            for resol in &d {
+                println!("  {}x{}  {:?}", resol.0, resol.1,
+                    camera.intervals(&format.format, *resol).unwrap());
+            }
+        } else {
+            println!("  {:?}", resolutions);
+        }
+    }
+}
+*/
